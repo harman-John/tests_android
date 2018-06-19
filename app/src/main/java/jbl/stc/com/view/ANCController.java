e2be054ccf24d7397513a37982f63892ed481ff8
Gan, John <John.Gan@harman.com>
2018-06-19 14:36:50 +0800

    add. permission and apk rename

--- app/src/main/java/jbl/stc/com/view/ANCController.java
+++ app/src/main/java/jbl/stc/com/view/ANCController.java
@@ -20,7 +20,6 @@ import android.view.SurfaceView;
 import jbl.stc.com.constant.JBLConstant;
 import jbl.stc.com.controller.ANCDrawManager;
 import jbl.stc.com.R;
-import jbl.stc.com.fragments.ANCFragment;
 
 public class ANCController extends SurfaceView {
     private static final String TAG = ANCController.class.getSimpleName();
