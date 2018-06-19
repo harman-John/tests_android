f3c3635aa15d52d01d8e8e056a40b25df232792f
Gan, John <John.Gan@harman.com>
2018-06-19 15:30:42 +0800

    move dashboard ui to headphone2

---
+++ app/src/main/java/jbl/stc/com/view/AppImageView.java
@@ -0,0 +1,42 @@
+package jbl.stc.com.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+
+public class AppImageView extends AppCompatImageView {
+    public AppImageView(Context context) {
+        super(context);
+    }
+
+    public AppImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AppImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //LogUtil.d("AppImageView", "onTouchEvent " + event.getAction());
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                setAlpha(0.5f);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                setAlpha(0.5f);
+                break;
+            case MotionEvent.ACTION_UP:
+                setAlpha(1f);
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                setAlpha(1f);
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+}
---
+++ app/src/main/java/jbl/stc/com/view/PoppinsRegularTextView.java
@@ -0,0 +1,34 @@
+package jbl.stc.com.view;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+
+
+public class PoppinsRegularTextView extends AppCompatTextView {
+
+    public PoppinsRegularTextView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public PoppinsRegularTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public PoppinsRegularTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+//        AssetManager assetManager = context.getAssets();
+//        Typeface typeface = Typeface.createFromAsset(assetManager, "fonts/Poppins-Regular.ttf");
+//        setTypeface(typeface);
+    }
+
+}
