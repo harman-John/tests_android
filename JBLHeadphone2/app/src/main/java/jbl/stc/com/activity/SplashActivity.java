0358f3a107a00114a122061e2e77a8e602bafac2
Gan, John <John.Gan@harman.com>
2018-07-03 11:38:44 +0800

    finish splash

--- JBLHeadphone2/app/src/main/java/jbl/stc/com/activity/SplashActivity.java
+++ JBLHeadphone2/app/src/main/java/jbl/stc/com/activity/SplashActivity.java
@@ -1,41 +1,14 @@
 package jbl.stc.com.activity;
 
-import android.app.PendingIntent;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.util.Log;
 
-import com.avnera.audiomanager.audioManager;
-import com.avnera.smartdigitalheadset.Bluetooth;
-import com.avnera.smartdigitalheadset.LightX;
 import com.avnera.smartdigitalheadset.Logger;
-import com.avnera.smartdigitalheadset.USB;
-
-import java.util.HashMap;
-import java.util.Iterator;
 
 import jbl.stc.com.R;
-import jbl.stc.com.constant.JBLConstant;
-import jbl.stc.com.dialog.AlertsDialog;
-import jbl.stc.com.fragment.BaseFragment;
-import jbl.stc.com.listener.AppUSBDelegate;
-import jbl.stc.com.storage.PreferenceKeys;
-import jbl.stc.com.storage.PreferenceUtils;
-import jbl.stc.com.utils.AppUtils;
-import jbl.stc.com.utils.StatusBarUtil;
 
-/**
- * BaseActivity
- * Created by darren.lu on 08/06/2017.
- */
+
 public class SplashActivity extends FragmentActivity  {
 
     private static final String TAG = SplashActivity.class.getSimpleName();
@@ -47,5 +20,6 @@ public class SplashActivity extends FragmentActivity  {
         setContentView(R.layout.activity_splash);
         Intent intent = new Intent(this, DashboardActivity.class);
         startActivity(intent);
+        finish();
     }
 }
