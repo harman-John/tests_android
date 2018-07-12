2add5e3778785f126e89287a437d2c9789cd1f98
Gan, John <John.Gan@harman.com>
2018-07-12 17:50:55 +0800

    first stage of connected before

--- JBLHeadphone2/app/src/main/java/jbl/stc/com/fragment/ConnectedBeforeFragment.java
+++ JBLHeadphone2/app/src/main/java/jbl/stc/com/fragment/ConnectedBeforeFragment.java
@@ -3,20 +3,17 @@ package jbl.stc.com.fragment;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothProfile;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v4.content.ContextCompat;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.TextPaint;
-import android.text.method.LinkMovementMethod;
-import android.text.style.ClickableSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.TextView;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,40 +33,76 @@ public class ConnectedBeforeFragment extends BaseFragment implements View.OnClic
     private ConnectedBeforeGridAdapter connectedBeforeGridAdapter;
     private GridView gridView;
 
+    public void stopTimerConnected(){
+        connectedBeforeGridAdapter.stopTimerConnected();
+    }
+
+    public void setSpecifiedDevice(BluetoothDevice bluetoothDevice){
+        Message msg = new Message();
+        msg.what = MSG_DEVICE_CONNECTED;
+        msg.obj = bluetoothDevice;
+        cbHandler.sendMessage(msg);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    private void startA2DPCheck(){
+        BluetoothAdapter mBtAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBtAdapter != null && mBtAdapter.isEnabled()) {
+            mBtAdapter.getProfileProxy(mContext, mListener, BluetoothProfile.A2DP);
+        }
+    }
+    List<BluetoothDevice> deviceList;
+    private BluetoothProfile.ServiceListener mListener = new BluetoothProfile.ServiceListener() {
+        @Override
+        public void onServiceConnected(int profile, BluetoothProfile proxy) {
+            if(profile == BluetoothProfile.A2DP) {
+                deviceList = proxy.getConnectedDevices();
+                Logger.d(TAG, " A2DP connected deviceList = "+deviceList +",size = "+deviceList.size());
+                cbHandler.sendEmptyMessage(MSG_UPDATE_UI);
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(int profile) {
+
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_connected_before,
                 container, false);
+        startA2DPCheck();
+        view.findViewById(R.id.relative_layout_connected_before_title).bringToFront();
+        view.findViewById(R.id.image_view_connected_before_white_plus).setOnClickListener(this);
+        gridView = view.findViewById(R.id.grid_view_connected_before);
 
-        Set<BluetoothDevice> pairedDevices = BluetoothAdapter.getDefaultAdapter().getBondedDevices();
-        Set<String> connectedBeforeDevices = PreferenceUtils.getStringSet(getContext(), PreferenceKeys.CONNECTED_BEFORE_DEVICES);
-        List<String> addLists = new ArrayList<>();
+        return view;
+    }
 
-        for (BluetoothDevice pairedDevice : pairedDevices) {
-            for (String deviceInfo : connectedBeforeDevices) {
-                if (deviceInfo.contains(pairedDevice.getAddress())) {
-                    addLists.add(deviceInfo);
-                }
+    public int connectedDeviceThroughA2dp(){
+        int connectedDeviceInc = 0;
+        for (ConnectedBeforeDevice connectedBeforeDevice: lists){
+            if (connectedBeforeDevice.a2dpConnected){
+                connectedDeviceInc ++;
             }
         }
+        return connectedDeviceInc;
+    }
 
-        connectedBeforeDevices.clear();
-        for (String removeDevice : addLists) {
-            connectedBeforeDevices.add(removeDevice);
-        }
-
-        gridView = view.findViewById(R.id.grid_view_connected_before);
+    List<ConnectedBeforeDevice> lists;
+    private void initList(){
+        lists = new ArrayList<>();
         Set<String> devicesSet = PreferenceUtils.getStringSet(getContext(), PreferenceKeys.CONNECTED_BEFORE_DEVICES);
-        Logger.i(TAG, "deviceSet = " + devicesSet + ",pairedDevices = " + pairedDevices);
-        List<ConnectedBeforeDevice> lists = new ArrayList<>();
+        Logger.i(TAG, "deviceSet = " + devicesSet);
         for (String value : devicesSet) {
             ConnectedBeforeDevice connectedBeforeDevice = new ConnectedBeforeDevice();
+            connectedBeforeDevice.a2dpConnected = false;
             if (value.toUpperCase().contains(JBLConstant.DEVICE_LIVE_650BTNC)) {
                 connectedBeforeDevice.deviceName = JBLConstant.DEVICE_LIVE_650BTNC;
                 connectedBeforeDevice.deviceIcon = ContextCompat.getDrawable(getActivity(), R.mipmap.everest_elite_700_icon);
@@ -100,10 +133,13 @@ public class ConnectedBeforeFragment extends BaseFragment implements View.OnClic
             }
             lists.add(connectedBeforeDevice);
         }
+    }
+
+    private void initView(){
+        initList();
         connectedBeforeGridAdapter = new ConnectedBeforeGridAdapter();
         connectedBeforeGridAdapter.setConnectedBeforeList(lists);
         gridView.setAdapter(connectedBeforeGridAdapter);
-        return view;
     }
 
     @Override
@@ -118,7 +154,43 @@ public class ConnectedBeforeFragment extends BaseFragment implements View.OnClic
                 switchFragment(new InfoFragment(), JBLConstant.SLIDE_FROM_RIGHT_TO_LEFT);
                 break;
             }
+            case R.id.image_view_connected_before_white_plus:{
+                getActivity().onBackPressed();
+                break;
+            }
         }
 
+    }
+
+    private CbHandler cbHandler = new CbHandler(Looper.getMainLooper());
+    private final static int MSG_UPDATE_UI = 0;
+    private final static int MSG_DEVICE_CONNECTED = 1;
+    private class CbHandler extends Handler{
+        public CbHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case MSG_UPDATE_UI:{
+                    initView();
+                    break;
+                }
+                case MSG_DEVICE_CONNECTED:{
+                    BluetoothDevice bluetoothDevice = ((BluetoothDevice)msg.obj);
+                    for(ConnectedBeforeDevice connectedBeforeDevice: lists){
+                        if (bluetoothDevice.getName().toUpperCase().contains(connectedBeforeDevice.deviceName)) {
+                            connectedBeforeDevice.a2dpConnected = true;
+                            connectedBeforeGridAdapter.setConnectedBeforeList(lists);
+                            break;
+                        }
+                    }
+                    stopTimerConnected();
+                    break;
+                }
+            }
+        }
     }
 }
